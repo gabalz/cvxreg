890f38c1032f484fe85c43f9efcdccf9368fc41c
Gabor Balazs <gabalz@gmail.com>
2025-01-06 20:09:58 +0100

    Tests for kernel regression.

--- algorithm/external/kernel_regression.py
+++ algorithm/external/kernel_regression.py
@@ -59,3 +59,33 @@ class KernelRegEstimator(Estimator):
             train=partial(kernel_reg_train, kernel=kernel, **kwargs),
             predict=kernel_reg_predict,
         )
+
+
+def _kernel_reg_l2_loss_tests():
+    """
+    >>> from common.util import set_random_seed
+    >>> set_random_seed(19)
+
+    >>> def regression_func(X):
+    ...     return 1.0 - 2.0*X[:, 0] + X[:, 1]**2
+    >>> X = np.random.randn(200, 2)
+    >>> y = regression_func(X) + 0.1 * np.random.randn(X.shape[0])
+
+    # L2-error of OLS is bigger than 6.
+    >>> X_test = np.random.randn(500, 2)
+    >>> y_test = regression_func(X_test)
+    >>> ols_model = np.linalg.lstsq(X.T.dot(X), X.T.dot(y), rcond=-1)[0]
+    >>> ols_yhat_test = np.sum(X_test * ols_model, axis=1)  # np.dot is not deterministic
+    >>> np.round(np.sum(np.square(ols_yhat_test - y_test)) / len(y_test), decimals=4)  # OLS out-of-sample L2-error
+    6.2752
+
+    >>> kreg1 = KernelRegEstimator('normal')
+    >>> model1 = kreg1.train(X, y)
+    >>> yhat1 = kreg1.predict(model1, X)
+    >>> np.round(np.mean(np.square(yhat1 - y)), decimals=4)  # in-sample L2-risk
+    0.033
+    >>> yhat1_test = kreg1.predict(model1, X_test)
+    >>> np.round(np.mean(np.square(yhat1_test - y_test)), decimals=4)  # out-of-sample L2-error
+    0.1667
+    """
+    pass
